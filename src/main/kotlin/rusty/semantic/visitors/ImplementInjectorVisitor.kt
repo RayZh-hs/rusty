248bb857010fc8fe089d520d0eefe0e823935e01
RayZh <rayzhangshanghai@hotmail.com>
2025-08-27 14:26:22 +0800

    feat: implement impl injection for enums

--- src/main/kotlin/rusty/semantic/visitors/ImplementInjectorVisitor.kt
+++ src/main/kotlin/rusty/semantic/visitors/ImplementInjectorVisitor.kt
@@ -1,23 +1,43 @@
 package rusty.semantic.visitors
 
+import rusty.core.CompileError
 import rusty.parser.nodes.ItemNode
 import rusty.semantic.support.Context
+import rusty.semantic.support.SemanticSymbol
 import rusty.semantic.visitors.bases.ScopeAwareVisitorBase
-import rusty.semantic.visitors.utils.getSemanticStructFromId
 import rusty.semantic.visitors.utils.getStructIdFromType
 import rusty.semantic.visitors.utils.injectAssociatedItems
+import rusty.semantic.visitors.utils.sequentialLookup
 
 class ImplementInjectorVisitor(override val ctx: Context) : ScopeAwareVisitorBase(ctx) {
 
     override fun visitInherentImplItem(node: ItemNode.ImplItemNode.InherentImplItemNode) {
-        val structIdentifier = getStructIdFromType(ctx, node.typeNode)
-        val semanticStruct = getSemanticStructFromId(ctx, structIdentifier, currentScope())
-        semanticStruct.injectAssociatedItems(ctx, node.associatedItems)
+        val identifier = getStructIdFromType(ctx, node.typeNode)
+        val semanticStruct = sequentialLookup(identifier, currentScope(), {it.structEnumST})
+            ?: throw CompileError("Cannot find struct or enum with id $identifier to implement").with(node)
+        when (semanticStruct.symbol) {
+            is SemanticSymbol.Struct -> {
+                semanticStruct.symbol.injectAssociatedItems(ctx, node.associatedItems)
+            }
+            is SemanticSymbol.Enum -> {
+                semanticStruct.symbol.injectAssociatedItems(ctx, node.associatedItems)
+            }
+            else -> throw CompileError("Expected struct or enum symbol, found ${semanticStruct.symbol}").with(node)
+        }
     }
 
     override fun visitTraitImplItem(node: ItemNode.ImplItemNode.TraitImplItemNode) {
-        val structIdentifier = getStructIdFromType(ctx, node.typeNode)
-        val semanticStruct = getSemanticStructFromId(ctx, structIdentifier, currentScope())
-        semanticStruct.injectAssociatedItems(ctx, node.associatedItems)
+        val identifier = getStructIdFromType(ctx, node.typeNode)
+        val semanticStruct = sequentialLookup(identifier, currentScope(), {it.structEnumST})
+            ?: throw CompileError("Cannot find struct or enum with id $identifier to implement").with(node)
+        when (semanticStruct.symbol) {
+            is SemanticSymbol.Struct -> {
+                semanticStruct.symbol.injectAssociatedItems(ctx, node.associatedItems)
+            }
+            is SemanticSymbol.Enum -> {
+                semanticStruct.symbol.injectAssociatedItems(ctx, node.associatedItems)
+            }
+            else -> throw CompileError("Expected struct or enum symbol, found ${semanticStruct.symbol}").with(node)
+        }
     }
 }
--- src/main/kotlin/rusty/semantic/visitors/utils/EnumUtils.kt
+++ src/main/kotlin/rusty/semantic/visitors/utils/EnumUtils.kt
@@ -1,27 +1,12 @@
 package rusty.semantic.visitors.utils
 
 import rusty.core.CompileError
-import rusty.lexer.Token
-import rusty.parser.nodes.TypeNode
 import rusty.parser.nodes.support.AssociatedItemsNode
 import rusty.semantic.support.Context
-import rusty.semantic.support.Scope
 import rusty.semantic.support.SemanticSymbol
 import kotlin.collections.set
 
-fun getStructIdFromType(ctx: Context, typeNode: TypeNode): String = when(typeNode) {
-    is TypeNode.TypePath -> {
-        if (typeNode.pathSegmentNode.token != Token.I_IDENTIFIER)
-            throw CompileError("Impl expected a struct identifier, found: ${typeNode.pathSegmentNode}")
-                .with(ctx).at(typeNode.pathSegmentNode.pointer)
-        typeNode.pathSegmentNode.name?: throw CompileError("Cannot implement an unnamed struct")
-            .with(ctx).at(typeNode.pathSegmentNode.pointer)
-    }
-    else -> throw CompileError("Cannot implement a non-path type: $typeNode")
-        .with(ctx).at(typeNode.pointer)
-}
-
-fun SemanticSymbol.Struct.injectAssociatedItems(ctx: Context, node: AssociatedItemsNode) {
+fun SemanticSymbol.Enum.injectAssociatedItems(ctx: Context, node: AssociatedItemsNode) {
     val implFunctions = node.functionItems
     val implConstants = node.constItems
     // Check for duplicates
